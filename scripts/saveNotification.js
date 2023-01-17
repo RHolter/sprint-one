8e48960d4e9f074f75f3f735544c36065e82f76d
Renee <renazuki@yahoo.com>
2023-01-16 18:38:39 -0800

    runs the save notification toast

---
+++ scripts/saveNotification.js
@@ -0,0 +1,8 @@
+// Run this when the page loads
+window.onload = () => {
+    const toastLiveExample = document.getElementById('saveNotification')
+    const toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+
+    console.log(toast)
+}
